3c57708bc14c76c1c810a85611fafa0a907bb716
Furqan <awais.shakir@decenternet.net>
2023-03-19 18:00:23 +0500

    new changes in main

---
+++ new.js
@@ -0,0 +1,19 @@
+console.log("Starting");
+
+var express = require('express');   
+
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(3000, function() {
+    console.log('Listening on port 3000');
+});
+
+app = express();
+
+app.use(express.static('public'));
